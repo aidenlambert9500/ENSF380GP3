8096c96a5dad6ace0eda203fbb4f419ed84bed7d
abdulrafay298 <abdul.rafay2@ucalgary.ca>
2024-02-14 19:53:39 -0700

    Finished Inquirer.java

--- edu/ucalgary/oop/Inquirer.java
+++ edu/ucalgary/oop/Inquirer.java
@@ -1,5 +1,31 @@
 package edu.ucalgary.oop;
 
 public class Inquirer {
-    
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String message;
+
+    public Inquirer(String firstName, String lastName, String phoneNumber, String message) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.message = message;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getServicesPhoneNum() {
+        return phoneNumber;
+    }
+
+    public String getInfo() {
+        return message;
+    }
 }
